e9f27af8e9532ec89d2a39652f44d72649e5990b
Jason Luo <luoyhang003@hotmail.com>
2015-09-10 11:46:53 +0800

    add connect database

---
+++ src/com/luoyuanhang/core/MainPage.java
@@ -0,0 +1,13 @@
+package com.luoyuanhang.core;
+
+import com.vaadin.server.VaadinRequest;
+import com.vaadin.ui.UI;
+
+/**
+ * Created by jason on 15/9/10.
+ */
+public class MainPage extends UI {
+    protected void init(VaadinRequest request){
+
+    }
+}
---
+++ src/com/luoyuanhang/dbconnect/DBConnector.java
@@ -0,0 +1,102 @@
+package com.luoyuanhang.dbconnect;
+
+
+import java.sql.*;
+
+/**
+ * Created by jason on 15/9/10.
+ *
+ * Provide the functions of the MySQL Database services.
+ *
+ * including connect to the database,
+ *           disconnect to the database,
+ *           search from database,
+ *           update data to database,etc.
+ *
+ *
+ */
+public class DBConnector {
+    //Database URL
+    public static final String DB_URL = "jdbc:mysql://127.0.0.1:3306/exmaple";
+    //Driver Name
+    public static final String DB_DRIVER = "com.mysql.jdbc.Driver";
+    //Database Username
+    public static final String DB_USERNAME = "root";
+    //Database Password
+    public static final String DB_PASSWORD = "haha123";
+
+
+    //create MySQL connection
+    private Connection connection = null;
+    //the times of excute query
+    private int count = 0;
+    //the set for saving result of query
+    private ResultSet resultSet = null;
+    //
+    private PreparedStatement preparedStatement = null;
+
+    /**
+     * Get connection from MySQL Database
+     * @return
+     */
+    public  Connection connect(){
+        try{
+            //Load Driver
+            Class.forName(DB_DRIVER).newInstance();
+            //Link to MySQL
+            connection = DriverManager.getConnection(DB_URL,DB_USERNAME,DB_PASSWORD);
+            //Output to Console
+            System.out.println("DATABASE: CONNECT SUCCESS!");
+        }catch(Exception exception){
+            exception.printStackTrace();
+            System.out.println("DATABASE: CONNECT FAIL!");
+        }finally {
+            return connection;
+        }
+    }
+
+
+    public boolean close(){
+        //flag of statement
+        boolean isClose = false;
+
+        //try to close result set
+        if(resultSet != null){
+            try{
+                resultSet.close();
+                resultSet = null;
+                System.out.println("DATABASE: CLOSE RESULT SET SUCCESS!");
+            }catch (SQLException e){
+                e.printStackTrace();
+                isClose = false;
+                System.out.println("DATABASE: CLOSE RESULT SET FAIL!");
+            }
+        }
+        //try to close prepared statement
+        if(preparedStatement != null){
+            try{
+                preparedStatement.close();
+                preparedStatement = null;
+                System.out.println("DATABASE: CLOSE PREPARED STATEMENT SUCCESS!");
+            }catch (SQLException e){
+                isClose = false;
+                e.printStackTrace();
+                System.out.println("DATABASE: CLOSE PREPARED STATEMENT SET FAIL!");
+            }
+        }
+        //try to close connection
+        if(connection != null){
+            try {
+                connection.close();
+                connection = null;
+                System.out.println("DATABASE: CLOSE CONNECTION SUCCESS!");
+            } catch (SQLException e) {
+                isClose = false;
+                e.printStackTrace();
+                System.out.println("DATABASE: CLOSE CONNECTION FAIL!");
+            }
+        }
+        return  isClose;
+    }
+
+}
