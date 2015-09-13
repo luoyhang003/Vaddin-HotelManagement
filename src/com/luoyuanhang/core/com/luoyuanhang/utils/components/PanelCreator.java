e928ef3d664bf6fffb8be3514ef7faffdd7bfd80
Jason Luo <luoyhang003@hotmail.com>
2015-09-13 14:37:10 +0800

    Register Window

--- src/com/luoyuanhang/core/com/luoyuanhang/utils/components/PanelCreator.java
+++ src/com/luoyuanhang/core/com/luoyuanhang/utils/components/PanelCreator.java
@@ -55,8 +55,6 @@ public class PanelCreator {
     }
 
     public static VerticalLayout createLoginWindow(){
-        String username = "";
-        String password = "";s
 
         VerticalLayout layout = new VerticalLayout();
 
@@ -70,25 +68,75 @@ public class PanelCreator {
         Label space = new Label();
         space.setWidth("500px");
 
-        TextField textfield_username = new TextField();
+        final TextField textfield_username = new TextField();
         textfield_username.setImmediate(true);
         textfield_username.setInputPrompt("用户名");
         textfield_username.setMaxLength(10);
 
-        PasswordField passwordField = new PasswordField();
+        final PasswordField passwordField = new PasswordField();
         passwordField.setImmediate(true);
         passwordField.setInputPrompt("********");
 
         Button login = new Button("登录", new Button.ClickListener() {
             @Override
             public void buttonClick(Button.ClickEvent clickEvent) {
+                String username = textfield_username.getValue();
+                String password = passwordField.getValue();
+
+
+
+
 
             }
         });
 
 
 
-        Button register = new Button("注册");
+        Button register = new Button("注册", new Button.ClickListener() {
+            @Override
+            public void buttonClick(Button.ClickEvent clickEvent) {
+                String username = "";
+                String password = "";
+                String name = "";
+                int sex = 0;
+                String tel = "";
+
+                Window window = new Window("注册");
+
+                FormLayout formLayout = new FormLayout();
+                formLayout.setSizeUndefined();
+
+                TextField username_tf = new TextField("*帐号：");
+                PasswordField password_pf = new PasswordField("*密码：");
+                TextField name_tf = new TextField("*姓名：");
+                OptionGroup sex_og = new OptionGroup("*性别：");
+                TextField tel_tf = new TextField("*电话：");
+                Button submit_btn = new Button("注册", new Button.ClickListener() {
+                    @Override
+                    public void buttonClick(Button.ClickEvent clickEvent) {
+
+                    }
+                });
+
+
+                formLayout.addComponent(username_tf);
+                formLayout.addComponent(password_pf);
+                formLayout.addComponent(name_tf);
+                formLayout.addComponent(sex_og);
+                formLayout.addComponent(tel_tf);
+                formLayout.addComponent(submit_btn);
+
+                window.setContent(formLayout);
+                window.setWidth(260.0f, Sizeable.Unit.PIXELS);
+                window.setDraggable(false);
+                window.setResizable(false);
+                window.setPosition((int)(Page.getCurrent().getBrowserWindowWidth()-window.getWidth())/2,
+                        200);
+
+                UI.getCurrent().addWindow(window);
+
+            }
+        });
 
 
 
